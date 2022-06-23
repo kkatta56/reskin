b1ea3e4a32195ab5d3ef12ad85ba7eca5935166b
Kaushik <kaushik.katta@gmail.com>
2022-06-23 11:28:55 -0400

    Update new_snake.py

--- new_snake.py
+++ new_snake.py
@@ -8,6 +8,8 @@ import csv
 
 from utils.dobot import *
 from reskin_sensor import ReSkinProcess
+from utils.force_sensor import ForceSensor, _ForceSensorSetting
+
 
 def save_data(buff_dat, mag_num, file_name):
     col_names = ['T', 'Bx', 'By', 'Bz']
@@ -25,22 +27,17 @@ def save_data(buff_dat, mag_num, file_name):
         csvwriter.writerow(fields)
         csvwriter.writerows(rows)
 
-def startDataProcess(robot, reskin_sensor, r, depth, filename, path="snake"):
-    # Start sensor stream and buffering data
-    reskin_sensor.start()
+def startSnakeProcess(robot, reskin_sensor, fs, r, depth, filename):
+    # Start buffering
     if reskin_sensor.is_alive():
         reskin_sensor.start_buffering()
         buffer_start = time.time()
     else:
         return "Error: stream has not started"
 
-    # Action during buffer
-    if path == "snake":
-        robot.startSnakePath(r,depth)
-    elif path == "updown":
-        robot.upDown(r,depth)
-    else:
-        return "Error: no valid path specified"
+    # Start snake path
+    print("Snake pattern started for iteration.")
+    robot.startSnakePath(r,depth)
 
     # Stop buffer
     reskin_sensor.pause_buffering()
@@ -48,10 +45,7 @@ def startDataProcess(robot, reskin_sensor, r, depth, filename, path="snake"):
 
     # Get buffered data
     buffered_data = reskin_sensor.get_buffer()
-
-    # Pause sensor stream
-    reskin_sensor.pause_streaming()
-    reskin_sensor.join()
+    ##############buffered_fsdata = fs.get_buffer()
 
     # Print buffered data summary
     if buffered_data is not None:
@@ -61,11 +55,12 @@ def startDataProcess(robot, reskin_sensor, r, depth, filename, path="snake"):
             )
         )
 
+    # FIGURE OUT SAVING FORCE SENSOR DATA
     save_data(buffered_data, reskin_sensor.num_mags, filename)
-
-
+    print("Iteration saved.")
 
 if __name__ == "__main__":
+    # Parse terminal command for arguments
     parser = argparse.ArgumentParser(
         description="Test code to run a ReSkin streaming process in the background. Allows data to be collected without code blocking"
     )
@@ -77,7 +72,21 @@ if __name__ == "__main__":
     # fmt: on
     args = parser.parse_args()
 
-    # Create sensor stream
+    # Initializing force sensor
+    fs_settings = _ForceSensorSetting(device_name_prefix="Dev",
+                                      device_ids=[1],
+                                      sensor_names=["FT34108"],
+                                      calibration_folder="./utils",
+                                      reverse_scaling={1: ["Fz"], 2: ["Fz"]},
+                                      # key: device_id, parameter. E.g.:if x & z dimension of sensor 1 and z dimension of sensor 2 has to be flipped use {1: ["Fx", "Fz"], 2: ["Fz"]}
+                                      remote_control=False, ask_filename=False, write_Fx=True,
+                                      write_Fy=True, write_Fz=True, write_Tx=False, write_Ty=False,
+                                      write_Tz=False, write_trigger1=True, write_trigger2=False,
+                                      zip_data=True, convert_to_forces=True,
+                                      priority='normal')
+    force_sensor = ForceSensor(fs_settings)
+
+    # Create and start sensor stream
     sensor_stream = ReSkinProcess(
         num_mags=args.num_mags,
         port=args.port,
@@ -86,9 +95,20 @@ if __name__ == "__main__":
         device_id=1,
         temp_filtered=args.temp_filtered,
     )
+    sensor_stream.start()
+    #################force_sensor.start_recording()
 
     # Initialize Dobot
     db = Dobot(port='/dev/ttyUSB0')
-    origin = [177.79611206054688, -95.56216430664062, -87.25672912597656]
-    depth = 8
-    startDataProcess(db, sensor_stream, origin, depth, "res_test.csv", path="snake")
+    origin = [177.29611206054688, -95.56216430664062, -87.25672912597656]
+
+    # Start data collection
+    depths = np.arange(5,6,0.2)
+    for i,depth in enumerate(depths):
+        startSnakeProcess(db, sensor_stream, force_sensor, origin, depth, "res_test_"+str(i)+".csv")
+    print("Finished data collection")
+
+    # Stop sensor stream
+    sensor_stream.pause_streaming()
+    #################force_sensor.pause_recording()
+    sensor_stream.join()
