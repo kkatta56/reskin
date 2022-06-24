e4e09798f01b84b0ed3931bbe0c6c2e0be04e048
Kaushik <kaushik.katta@gmail.com>
2022-06-24 15:29:34 -0400

    Update new_snake.py

--- new_snake.py
+++ new_snake.py
@@ -1,4 +1,6 @@
-#python test.py -p /dev/ttyACM0
+### find ports:
+# conda install pyserial
+# python -m serial.tools.list_ports
 
 import numpy as np
 import argparse
@@ -60,17 +62,6 @@ def startSnakeProcess(robot, reskin_sensor, fs, r, depth, filename):
     print("Iteration saved.")
 
 if __name__ == "__main__":
-    # Parse terminal command for arguments
-    parser = argparse.ArgumentParser(
-        description="Test code to run a ReSkin streaming process in the background. Allows data to be collected without code blocking"
-    )
-    # fmt: off
-    parser.add_argument("-p", "--port", type=str, help="port to which the microcontroller is connected", required=True,)
-    parser.add_argument("-b", "--baudrate", type=str, help="baudrate at which the microcontroller is streaming data", default=115200,)
-    parser.add_argument("-n", "--num_mags", type=int, help="number of magnetometers on the sensor board", default=5,)
-    parser.add_argument("-tf", "--temp_filtered", action="store_true", help="flag to filter temperature from sensor output",)
-    # fmt: on
-    args = parser.parse_args()
 
     # Initializing force sensor
     fs_settings = _ForceSensorSetting(device_name_prefix="Dev",
@@ -86,29 +77,36 @@ if __name__ == "__main__":
                                       priority='normal')
     force_sensor = ForceSensor(fs_settings)
 
-    # Create and start sensor stream
-    sensor_stream = ReSkinProcess(
-        num_mags=args.num_mags,
-        port=args.port,
-        baudrate=args.baudrate,
-        burst_mode=True,
-        device_id=1,
-        temp_filtered=args.temp_filtered,
-    )
-    sensor_stream.start()
-    #################force_sensor.start_recording()
-
     # Initialize Dobot
     db = Dobot(port='/dev/ttyUSB0')
-    origin = [177.29611206054688, -95.56216430664062, -87.25672912597656]
+    #################force_sensor.start_recording()
 
-    # Start data collection
-    depths = np.arange(5,6,0.2)
-    for i,depth in enumerate(depths):
-        startSnakeProcess(db, sensor_stream, force_sensor, origin, depth, "res_test_"+str(i)+".csv")
-    print("Finished data collection")
+    # Create and start sensor streams
+    port_names = ['/dev/ttyACM0','/dev/ttyACM1','/dev/ttyACM2']
+    origins = [[177.29611206054688, -197.7755126953125, -87.25672912597656],
+               [177.29611206054688, -95.56216430664062, -87.25672912597656],
+               [177.29611206054688, 77.446216430664062, -87.25672912597656]]
 
-    # Stop sensor stream
-    sensor_stream.pause_streaming()
-    #################force_sensor.pause_recording()
-    sensor_stream.join()
+    # Iterate over each port/origin
+    for i,port in enumerate(port_names):
+        sensor_stream = ReSkinProcess(
+            num_mags=5,
+            port=port,
+            baudrate=115200,
+            burst_mode=True,
+            device_id=1,
+            temp_filtered=False,
+        )
+        print("Using port: "+port)
+        sensor_stream.start()
+
+        # Start data collection
+        depths = [5, 5.5, 6]
+        for j,depth in enumerate(depths):
+            startSnakeProcess(db, sensor_stream, force_sensor, origins[i], depth, "port_"+str(i+1)+"_depth_"+str(j+1)+".csv")
+        print("Finished data collection")
+
+        # Stop sensor stream
+        sensor_stream.pause_streaming()
+        #################force_sensor.pause_recording()
+        sensor_stream.join()
