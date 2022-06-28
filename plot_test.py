4c80b22045544ed907d6f65a5ddd9e304d3d10de
Kaushik <kaushik.katta@gmail.com>
2022-06-28 16:10:17 -0400

    Update plot_test.py

--- plot_test.py
+++ plot_test.py
@@ -1,18 +1,23 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import csv
 
-x = []
-y = []
-z = []
+def openFile(filename):
+    data = []
+    with open(filename) as csvfile:
+        reader = csv.reader(csvfile)
+        for rid, row in enumerate(reader):
+            if rid == 0:
+                column_names = row
+            else:
+                desired_array = [float(numeric_string) for numeric_string in row]
+                data.append(desired_array)
+    return data, column_names
 
-with open('res_test.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        x.append(row['Bx0'])
-        y.append(row['By0'])
-        z.append(row['Bz0'])
-
-plt.plot(x)
+d, col_names = openFile('touch_1.csv')
+proc_df = pd.DataFrame(d, columns=col_names)
+proc_df.plot(y="Bx0", kind="line")
 plt.show()
+print(proc_df['Bz0'])
 
