1e56412a7bf296343948d5e5451160f928baea8f
Kaushik <kaushik.katta@gmail.com>
2022-07-11 16:44:34 -0400

    Update model_test.py

--- model_test.py
+++ model_test.py
@@ -8,7 +8,7 @@ class ResDataSet(Dataset):
     def __init__(self, arr):
         self.b_vals = arr[:, 0:15]
         self.x_loc = arr[:, 16]
-        #self.y_loc = arr[:, 17]
+        self.y_loc = arr[:, 17]
 
     def __len__(self):
         return len(self.x_loc)
@@ -16,13 +16,20 @@ class ResDataSet(Dataset):
     def __getitem__(self, idx):
         b_vals = self.b_vals[idx]
         x_loc = self.x_loc[idx]
-        #y_loc = self.y_loc[idx]
-        return b_vals, x_loc
+        y_loc = self.y_loc[idx]
+        return b_vals, x_loc, y_loc
 
 class MLP(nn.Module):
     def __init__(self):
         super().__init__()
-        self.layers = nn.Sequential(
+        self.layers1 = nn.Sequential(
+            nn.Linear(15, 500),
+            nn.ReLU(),
+            nn.Linear(500, 500),
+            nn.ReLU(),
+            nn.Linear(500, 1)
+        )
+        self.layers2 = nn.Sequential(
             nn.Linear(15, 500),
             nn.ReLU(),
             nn.Linear(500, 500),
@@ -31,20 +38,20 @@ class MLP(nn.Module):
         )
 
     def forward(self, x):
-        return self.layers(x)
+        return self.layers1(x), self.layers2(x)
 
 
 if __name__ == '__main__':
 
-    batch_size = 50
+    batch_size = 10
 
-    full_dataset = ResDataSet(pd.read_csv('datasets/normalized/port_2_depth_1.csv').to_numpy())
+    #full_dataset = ResDataSet(pd.read_csv('datasets/normalized/port_2_depth_1.csv').to_numpy())
     train_size = int(0.8 * len(full_dataset))
     test_size = len(full_dataset) - train_size
     train_dataset, test_dataset = torch.utils.data.random_split(full_dataset, [train_size, test_size])
 
-    #train_dataset = ResDataSet(pd.read_csv('datasets/normalized/port_2_depth_1.csv').to_numpy())
-    #test_dataset = ResDataSet(pd.read_csv('datasets/normalized/port_3_depth_1.csv').to_numpy())
+    train_dataset = ResDataSet(pd.read_csv('datasets/normalized/port_2_depth_1.csv').to_numpy())
+    test_dataset = ResDataSet(pd.read_csv('datasets/normalized/port_3_depth_1.csv').to_numpy())
 
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                                batch_size=batch_size,
@@ -72,18 +79,20 @@ if __name__ == '__main__':
         # Iterate over the DataLoader for training data
         for i, data in enumerate(train_loader, 0):
             # Get and prepare inputs
-            inputs, x_loc = data
-            inputs, x_loc = inputs.float(), x_loc.float()
-            x_loc = x_loc.reshape((x_loc.shape[0], 1))
+            inputs, x_loc, y_loc = data
+            inputs, x_loc, y_loc = inputs.float(), x_loc.float(), y_loc.float()
+            x_loc, y_loc = x_loc.reshape((x_loc.shape[0], 1)), y_loc.reshape((y_loc.shape[0], 1))
 
             # Zero the gradients
             optimizer.zero_grad()
 
             # Perform forward pass
-            outputs = mlp(inputs)
+            x_val, y_val = mlp(inputs)
 
             # Compute loss
-            loss = loss_function(outputs, x_loc)
+            loss_x = loss_function(x_val, x_loc)
+            loss_y = loss_function(y_val, y_loc)
+            loss = loss_x + loss_y
 
             # Perform backward pass
             loss.backward()
@@ -104,14 +113,16 @@ if __name__ == '__main__':
 # Test the model
 # In test phase, we don't need to compute gradients (for memory efficiency)
 with torch.no_grad():
-    correct = 0
-    total = 0
-    for inputs, x_loc in test_loader:
-        inputs, x_loc = inputs.float(), x_loc.float()
-        predicted = mlp(inputs)
-        predicted = np.reshape(predicted, -1)
-        print(predicted, x_loc)
+    x_correct, y_correct, tot_correct, total = 0, 0, 0, 0
+    for inputs, x_loc, y_loc in test_loader:
+        inputs, x_loc, y_loc = inputs.float(), x_loc.float(), y_loc.float()
+        x_pred, y_pred = mlp(inputs)
+        x_pred, y_pred = np.reshape(x_pred, -1), np.reshape(y_pred, -1)
         total += inputs.size(0)
         tolerance = 0.5
-        correct += ((x_loc-tolerance <= predicted) & (predicted <= x_loc+tolerance)).sum().item()
-    print('Accuracy of the network on the test values: {} %'.format(100 * correct / total))
+        x_correct += ((x_loc-tolerance <= x_pred) & (x_pred <= x_loc+tolerance)).sum().item()
+        y_correct += ((y_loc - tolerance <= y_pred) & (y_pred <= y_loc + tolerance)).sum().item()
+        tot_correct += ((x_loc-tolerance <= x_pred) & (x_pred <= x_loc+tolerance) & (y_loc - tolerance <= y_pred) & (y_pred <= y_loc + tolerance)).sum().item()
+    print('X value accuracy of the network on the test values: {} %'.format(100 * x_correct / total))
+    print('Y value accuracy of the network on the test values: {} %'.format(100 * y_correct / total))
+    print('Accuracy of the network on the test values: {} %'.format(100 * tot_correct / total))
