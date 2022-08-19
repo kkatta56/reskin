3b7a43f85f24e5a85e7b74463fb0a3c2030decd9
kaushik katta <kaushik.katta@gmail.com>
2022-08-19 12:43:20 -0400

    final code update 8/19

--- plot_test.py
+++ plot_test.py
@@ -19,18 +19,24 @@ def readData(filename):
     # Return ReSkin Data and Force Data arrays
     return np.array(res_data), np.array(force_data)
 
-# Read data
-time_string = '08_05_2022_10:31:21'
-r1, f1 = readData('datasets/'+time_string+'/normalized/port_1_depth_1.npz')
-r2, f2 = readData('datasets/'+time_string+'/normalized/port_2_depth_1.npz')
-r3, f3 = readData('datasets/'+time_string+'/normalized/port_3_depth_1.npz')
+var = 'magnet'
+port = 1
+data_type = 'normalized'
+r1, f1 = readData('datasets/'+var+'_base/'+data_type+'/port_'+str(port)+'_depth_1.npz')
+r2, f2 = readData('datasets/'+var+'_switch_1_and_2/'+data_type+'/port_'+str(port)+'_depth_1.npz')
+r3, f3 = readData('datasets/'+var+'_switch_1_and_3/'+data_type+'/port_'+str(port)+'_depth_1.npz')
+r4, f4 = readData('datasets/'+var+'_switch_2_and_3/'+data_type+'/port_'+str(port)+'_depth_1.npz')
 
+
+columns = ['Bx0', 'By0', 'Bz0', 'Bx1', 'By1', 'Bz1', 'Bx2', 'By2', 'Bz2',
+           'Bx3', 'By3', 'Bz3', 'Bx4', 'By4', 'Bz4']
 # Plot data
-column = 0
-plt.plot(r1[:,column], label="Skin 1")
-plt.plot(r2[:,column], label="Skin 2")
-plt.plot(r3[:,column], label="Skin 3")
-plt.title("original setup")
-plt.legend()
-plt.show()
+for column in range(15):
+    plt.plot(r2[:, column], label="switch 1 and 2")
+    plt.plot(r3[:, column], label="switch 1 and 3")
+    plt.plot(r4[:, column], label="switch 2 and 3")
+    plt.plot(r1[:, column], label="original")
+    plt.title(str(columns[column]))
+    plt.legend()
+    plt.show()
 
